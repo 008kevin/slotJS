ee26deca4605f3873fed5f80932037361880b130
SzaZoa1227 <szabozoard27@gmail.com>
2025-05-27 23:40:44 +0200

    utallak js

---
+++ src/js/index.ts
@@ -0,0 +1,20 @@
+function generateNewRandoms(): number[] {
+    let randomNums: number[] = [];
+    let colNum : number = document.querySelectorAll<HTMLElement>(".col").length;
+    for (let i = 0; i < colNum; i++) {
+        randomNums.push(Math.floor(Math.random() * 20));
+    }
+    return randomNums;
+}
+
+const spin : HTMLButtonElement = document.querySelectorAll<HTMLButtonElement>(".button")[0];
+
+if (spin) {
+    spin.addEventListener("click", () => {
+        let randomNums : number[] = generateNewRandoms();1
+        let cols = document.querySelectorAll<HTMLElement>(".col");
+        for (let i = 0; i < cols.length; i++) {
+            cols[i].innerText = randomNums[i].toString();
+        }
+    });
+}

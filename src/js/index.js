3d1532c6d7b88697dcbc16d5fc7487bc02378ce4
SzaZoa1227 <szabozoard27@gmail.com>
2025-05-27 23:25:52 +0200

    valahogy igy

--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,17 @@
-let randomNums = [] // 9 random szam 
 
 function generateNewRandoms() {
+    let randomNums = [] // 9 random szam 
     for (let i = 0; i < 9; i++) {
-        Math.floor(Math.random() * 20);
+        randomNums.Push(Math.floor(Math.random() * 20));
     }
+    return randomNums;
 }
 
+let spinBtn = document.querySelectorAll(".button")[0];
+spinBtn.addEventListener("click", () => {
+    let randomNums = generateNewRandoms();
+    for (let i = 0; i < 9; i++) {
+        document.querySelectorAll(".col")[i].innerText = randomNums[i];
+    }
+    
+});

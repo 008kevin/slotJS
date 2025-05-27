7c15b2f3392c3d0ace3829b0dc47eb87717f44ca
Viuviu42 <lengyelcsongi@gmail.com>
2025-05-27 19:44:11 +0200

    eltárolása a pénz változásnak

--- src/js/money.js
+++ src/js/money.js
@@ -1,14 +1,28 @@
 let money;
+let transactions = [];
+
 if (localStorage.getItem("money") != null) {
     money = localStorage.getItem("money");
+    transactions = localStorage.getItem("")
 } else {
     money = 1000; // alap pénz
+    transactions.push(money)
 }
 
 function removeMoney(amount) {
     money += amount;
+    transactions.push(money)
+    localStorage.setItem("transactions", JSON.stringify(transactions))
 }
 
 function addMoney(amount) {
     money += amount;
+    transactions.push(money)
+    localStorage.setItem("transactions", JSON.stringify(transactions))
+}
+
+function getTransactions(){
+transactions.forEach((p) => {
+console.log(p)
+})
 }

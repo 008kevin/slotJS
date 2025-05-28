5121b271404b46acada11d25473ee10fc9ea6c92
008kevin <77583631+008kevin@users.noreply.github.com>
2025-05-28 14:01:29 +0200

    translateToIcon

---
+++ src/js/numberToIcon.js
@@ -0,0 +1,16 @@
+let icons = {
+    1: "fa-bomb",
+    2: "fa-heart",
+    3: "fa-money-bill",
+    4: "fa-fire",
+    5: "fa-snowflake",
+    6: "fa-flag",
+    7: "fa-leaf",
+    8: "fa-crown",
+    9: "fa-cube",
+    10: "fa-toilet-paper"
+}
+
+function translateToIcon(number) {
+    return number in icons ? icons[number] : "fa-square";
+}

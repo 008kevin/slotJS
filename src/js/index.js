13a00bbcba8c926644e2b99a8594f9eedc23ae2e
SzaZoa1227 <116081320+SzaZoa1227@users.noreply.github.com>
2025-05-28 09:10:05 +0200

    push compiled ts

---
+++ src/js/index.js
@@ -0,0 +1,20 @@
+"use strict";
+function generateNewRandoms() {
+    let randomNums = [];
+    let colNum = document.querySelectorAll(".col").length;
+    for (let i = 0; i < colNum; i++) {
+        randomNums.push(Math.floor(Math.random() * 20));
+    }
+    return randomNums;
+}
+const spin = document.querySelectorAll(".button")[0];
+if (spin) {
+    spin.addEventListener("click", () => {
+        let randomNums = generateNewRandoms();
+        1;
+        let cols = document.querySelectorAll(".col");
+        for (let i = 0; i < cols.length; i++) {
+            cols[i].innerText = randomNums[i].toString();
+        }
+    });
+}
---
+++ src/js/money.js
@@ -0,0 +1,41 @@
+let money;
+let transactions = [];
+if (localStorage.getItem("money") !== null) {
+    money = parseFloat(localStorage.getItem("money"));
+    transactions = JSON.parse(localStorage.getItem("transactions")) || [];
+}
+else {
+    money = 1000;
+    transactions.push(money);
+}
+function removeMoney(amount) {
+    money -= amount;
+    transactions.push(money);
+    localStorage.setItem("money", money.toString());
+    localStorage.setItem("transactions", JSON.stringify(transactions));
+}
+function addMoney(amount) {
+    money += amount;
+    transactions.push(money);
+    localStorage.setItem("money", money.toString());
+    localStorage.setItem("transactions", JSON.stringify(transactions));
+}
+function getTransactions() {
+    transactions.forEach((p) => {
+        console.log(p);
+    });
+}
+let table = document.querySelectorAll(".table")[0];
+let resultButton = document.querySelectorAll(".button")[1];
+resultButton === null || resultButton === void 0 ? void 0 : resultButton.addEventListener("click", (index) => {
+    if (table) {
+        transactions.forEach((transaction, index) => {
+            let row = table === null || table === void 0 ? void 0 : table.insertRow(0);
+            let cell1 = row === null || row === void 0 ? void 0 : row.insertCell(0);
+            let cell2 = row === null || row === void 0 ? void 0 : row.insertCell(1);
+            cell1.innerText = (index + 1).toString();
+            cell2.innerText = transaction.toString();
+        });
+    }
+});
+

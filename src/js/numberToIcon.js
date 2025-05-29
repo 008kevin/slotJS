97847583dfa49d14fa6cbd412fa401e4b2cabc8b
008kevin <kardoskevin07@gmail.com>
2025-05-29 20:53:43 +0200

    kevesbe rigged multiplierek

--- src/js/numberToIcon.js
+++ src/js/numberToIcon.js
@@ -1,9 +1,9 @@
 let icons = [
     {icon: "fa-skull", multiplier: 0.1}, // 0.1 db5
     {icon: "fa-bomb", multiplier: 0.5},  // 0.5 db4
-    {icon: "fa-heart", multiplier: 1}, // 1 db3
-    {icon: "fa-money-bill", multiplier: 1.5}, // 1.5 db2
-    {icon: "fa-crown", multiplier: 2}, // 2 db1
+    {icon: "fa-heart", multiplier: 2}, // 1 db3
+    {icon: "fa-money-bill", multiplier: 5}, // 1.5 db2
+    {icon: "fa-crown", multiplier: 10}, // 2 db1
 ]
 
 function translateToIcon(number) {

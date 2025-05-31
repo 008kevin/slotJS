c72488f287f845fab71e4031f672de0ef9c82325
SzaZoa1227 <szabozoard27@gmail.com>
2025-05-31 15:00:43 +0200

    looks

--- src/js/numberToIcon.js
+++ src/js/numberToIcon.js
@@ -1,6 +1,6 @@
 let icons = [
     {icon: "fa-skull", multiplier: 0.1}, // 0.1 db5
-    {icon: "fa-bomb", multiplier: 1.1},  // 0.5 db4
+    {icon: "fa-bomb", multiplier: 1.1},  // 1.1 db4
     {icon: "fa-heart", multiplier: 2}, // 1 db3
     {icon: "fa-money-bill", multiplier: 5}, // 1.5 db2
     {icon: "fa-crown", multiplier: 10}, // 2 db1
